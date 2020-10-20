a8b47ca7d05b1042e359d61e09d059d13c7684b6
Bruno Zorić <bruno.zoric@gmail.com>
2020-10-20 18:34:28 +0200

    rewrite connected store and add recoil-undo

--- src/ConnectedStore.ts
+++ src/ConnectedStore.ts
@@ -5,8 +5,10 @@ import {
 	RecoilState,
 	selector,
 	SetterOrUpdater,
+    useRecoilValue,
 	useSetRecoilState
 } from "recoil";
+import { useBatching, useRedo, useUndo } from "recoil-undo";
 
 
 /**
@@ -16,38 +18,99 @@ import {
  * that calls updateInContext and passes itself as the context.
  * @see RecoilRootWithStore
  */
-class ConnectedStore{
-	private store:{[key:string]: { setter:SetterOrUpdater<any>|null, state:RecoilState<any> }};
-	constructor() {
-		this.store = {};
-	}
-	
-	addRecoilState<T>(rs:RecoilState<T>):RecoilState<T>{
-		this.store[rs.key] = {state:rs, setter:null};
-		return rs;
-	}
-	
-	updateRecoilState<T>(a:RecoilState<T>, newVal:T){
-		if(!this.store[a.key])
-			throw new Error('Attempting to update a connected value that was not created via connectedAtom');
-		else {
-			if(this.store[a.key].setter){
-				// @ts-ignore
-				this.store[a.key].setter(newVal);
-			}else{
-				throw new Error('Attempting to update a connected value, that has no setter attached yet. Using RecoilRootWithStore?');
-			}
-		}
-	}
-	public updateInContext(context:any){
-		var hook = useSetRecoilState;
-		const update = (st:{ setter:SetterOrUpdater<any>|null, state:RecoilState<any> })=>{
-			st.setter = hook(st.state);
-		};
-		for (let storeKey in this.store) {
-			update.apply(context,[this.store[storeKey]]);
-		}
-	}
+type ConnectedStoreType = {
+    setter: SetterOrUpdater<any> | null;
+    value: any;
+    state: RecoilState<any>;
+};
+type HelpersType = "startBatch" | "endBatch" | "undo" | "redo";
+
+class ConnectedStore {
+    private readonly _store: Map<string, ConnectedStoreType> = new Map();
+    private readonly _helpers: Map<HelpersType, () => void> = new Map();
+    
+    public addRecoilState<T>(state: RecoilState<T>): RecoilState<T> {
+        this._store.set(state.key, {
+            state: state,
+            setter: null,
+            value: null
+        });
+        return state;
+    }
+    
+    public updateRecoilState<T>(a: RecoilState<T>, newVal: T) {
+        const cs = this._store.get(a.key);
+        if (!cs) {
+            throw new Error(
+                "Attempting to update a connected value that was not created via connectedAtom."
+            );
+        }
+        if (!cs.setter) {
+            throw new Error(
+                "Attempting to update a connected value, that has no setter attached yet. Using RecoilRootWithStore?"
+            );
+        }
+        cs.setter(newVal);
+    }
+    
+    public getRecoilState<T>(state: RecoilState<T>): T {
+        const cs = this._store.get(state.key);
+        if (!cs) {
+            throw new Error(
+                "Attempting to get a connected value that was not created via connectedAtom."
+            );
+        }
+        if (!cs.value) {
+            throw new Error(
+                "Attempting to get a connected value, that has no value attached yet. Using RecoilRootWithStore?"
+            );
+        }
+        return cs.value;
+    }
+    
+    public updateInContext(context: any): void {
+        const batching = useBatching;
+        const setHook = useSetRecoilState;
+        const getHook = useRecoilValue;
+        const update = (store: ConnectedStoreType) => {
+            store.setter = setHook(store.state);
+        };
+        const value = function(store: ConnectedStoreType) {
+            store.value = getHook(store.state);
+        };
+        
+        for (const store of this.getStoreValues()) {
+            update.apply(context, [store]);
+            value.apply(context, [store]);
+        }
+        
+        this._helpers.clear();
+        const { startBatch, endBatch } = batching.apply(context);
+        this._helpers.set("startBatch", startBatch);
+        this._helpers.set("endBatch", endBatch);
+        this._helpers.set("undo", useUndo.apply(context));
+        this._helpers.set("redo", useRedo.apply(context));
+    }
+    
+    public startBatch(): void {
+        this._helpers.get("startBatch");
+    }
+    
+    public endBatch(): void {
+        this._helpers.get("endBatch");
+    }
+    
+    public undo(): void {
+        this._helpers.get("undo");
+    }
+    
+    public redo(): void {
+        this._helpers.get("redo");
+    }
+    
+    private getStoreValues(): ConnectedStoreType[] {
+        return Array.from(this._store.values());
+    }
 }
 
 export const connectedStore:ConnectedStore = new ConnectedStore();
@@ -82,4 +145,24 @@ export function connectedAtom<T>(options: AtomOptions<T>): RecoilState<T>{
  */
 export function connectedSelector<T>(options: ReadWriteSelectorOptions<T>): RecoilState<T>{
 	return connectedStore.addRecoilState(selector<T>(options) as RecoilState<T>); //store the atom for future reference
+}
+
+export const connectedAtomValue = <T>(state: RecoilState<T>): T => {
+    return connectedStore.getRecoilState<T>(state);
+}
+
+export const connectedBatchStart = (): void => {
+    connectedStore.startBatch();
+}
+
+export const connectedBatchEnd = (): void => {
+    connectedStore.endBatch();
+}
+
+export const connectedUndo = (): void => {
+    connectedStore.undo();
+}
+
+export const connectedRedo = (): void => {
+    connectedStore.redo();
 }
